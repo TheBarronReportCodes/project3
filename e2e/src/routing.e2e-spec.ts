3bae1eccb91bd01aa6964272e2353e203e54c83a
jbarron8 <jbarron8@cs6261-vm26.uwg.westga.edu>
2020-07-01 11:24:15 -0400

    routing.e2e-spec.ts file created. and practice test passed. Beginning code to navigate to welcome page

---
+++ e2e/src/routing.e2e-spec.ts
@@ -0,0 +1,9 @@
+import { browser, logging } from 'protractor';
+
+describe('Protractor Demo App', function() {
+  it('should have a title', function() {
+    browser.get('http://juliemr.github.io/protractor-demo/');
+
+    expect(browser.getTitle()).toEqual('Super Calculator');
+  });
+});
