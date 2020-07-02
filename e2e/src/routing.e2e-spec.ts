24205722e9c153080f4a6e2b0fe5f67e5a5bb733
jbarron8 <jbarron8@cs6261-vm06.uwg.westga.edu>
2020-07-01 20:44:51 -0400

    separated test three into two tests. Moving to part two

--- e2e/src/routing.e2e-spec.ts
+++ e2e/src/routing.e2e-spec.ts
@@ -16,8 +16,19 @@ describe('Routing Tests', function() {
     expect(result.getText()).toEqual('Register Players');
   });
 
-  it('should navigate to brackets page and back to welcome page', function() {
+  it('should navigate to brackets page', function() {
     browser.get('/');
+    element(by.id('bracketsLink')).click();
+    var result = element(by.id('subpageTitle'));
+    expect(result.getText()).toEqual('Brackets');
+  });
+
+  it('should navigate from a different page back to the welcome page', function() {
+    browser.get('/');
+    element(by.id('registrationLink')).click();
+    var result0 = element(by.id('subpageTitle'));
+    expect(result0.getText()).toEqual('Register Players');
+
     element(by.id('bracketsLink')).click();
     var result1 = element(by.id('subpageTitle'));
     expect(result1.getText()).toEqual('Brackets');
