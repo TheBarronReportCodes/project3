ef0360e5676ca351ee358f8fd541d03e0badb82a
jbarron8 <jbarron8@cs6261-vm28.uwg.westga.edu>
2020-07-02 12:34:59 -0400

    part two first test passed of inputting values in input field and checking to make sure the value of the input field was succesfully updated

--- e2e/src/success.e2e-spec.ts
+++ e2e/src/success.e2e-spec.ts
@@ -4,14 +4,13 @@ describe('Successful Scenario Tests', function() {
   it('should update players array on registration page', function() {
     browser.get('/');
     element(by.id('registrationLink')).click();
-    var input1 = element(by.model(players[0]));
+
+    var input1 = element(by.id('contestant0'));
     input1.sendKeys('Goku');
+    expect(input1.getAttribute('value')).toEqual('Goku');
 
-    var input2 = element(by.model(players[1]));
+    var input2 = element(by.id('contestant1'));
     input2.sendKeys('Vegeta');
-
-    var result = element(by.id('contestant1'));
-
-    expect(result.getText()).toEqual("Vegeta");
+    expect(input2.getAttribute('value')).toEqual('Vegeta');
   });
 });
