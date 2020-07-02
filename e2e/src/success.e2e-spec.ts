c16fc313854fe3e9d74d28157de4cbd773990758
jbarron8 <jbarron8@cs6261-vm28.uwg.westga.edu>
2020-07-02 13:40:02 -0400

    Writing tests for autofill buttons. Autofill test of two players complete

--- e2e/src/success.e2e-spec.ts
+++ e2e/src/success.e2e-spec.ts
@@ -1,33 +1,51 @@
 import { browser, element, by, logging } from 'protractor';
 
 describe('Successful Scenario Tests', function() {
-  it('should update players array on registration page', function() {
-    browser.get('/');
-    element(by.id('registrationLink')).click();
+  describe('Input values and registerContestants()', function() {
+    it('should update players array on registration page', function() {
+      browser.get('/');
+      element(by.id('registrationLink')).click();
 
-    var input1 = element(by.id('contestant0'));
-    input1.sendKeys('Goku');
-    expect(input1.getAttribute('value')).toEqual('Goku');
+      var input1 = element(by.id('contestant0'));
+      input1.sendKeys('Goku');
+      expect(input1.getAttribute('value')).toEqual('Goku');
 
-    var input2 = element(by.id('contestant1'));
-    input2.sendKeys('Vegeta');
-    expect(input2.getAttribute('value')).toEqual('Vegeta');
+      var input2 = element(by.id('contestant1'));
+      input2.sendKeys('Vegeta');
+      expect(input2.getAttribute('value')).toEqual('Vegeta');
+    });
+
+    it('should register contestants successfully', function() {
+      browser.get('/');
+      element(by.id('registrationLink')).click();
+
+      var input1 = element(by.id('contestant0'));
+      input1.sendKeys('Goku');
+      var input2 = element(by.id('contestant1'));
+      input2.sendKeys('Vegeta');
+
+      element(by.id('registerContestantsButton')).click();
+      var result = element(by.id('registrationMessage'));
+      expect(result.getText()).toEqual('Goku,Vegeta');
+    });
+
   });
 
-  it('should register contestants successfully', function() {
-    browser.get('/');
-    element(by.id('registrationLink')).click();
+  describe('Autofill Tests', function() {
+    it('should successfully autofill two players', function() {
+      browser.get('/');
+      element(by.id('registrationLink')).click();
 
-    var input1 = element(by.id('contestant0'));
-    input1.sendKeys('Goku');
-    var input2 = element(by.id('contestant1'));
-    input2.sendKeys('Vegeta');
+      element(by.id('autofillTwoButton')).click();
 
-    element(by.id('registerContestantsButton')).click();
-    var result = element(by.id('registrationMessage'));
-    expect(result.getText()).toEqual('Goku,Vegeta');
-  });
+      var input1 = element(by.id('contestant0'));
+      var input2 = element(by.id('contestant1'));
+      expect(input1.getAttribute('value')).toEqual('Zoe');
+      expect(input2.getAttribute('value')).toEqual('Kaylee');
 
-  it('should successfully autofill players', function() {
+      element(by.id('registerContestantsButton')).click();
+      var result = element(by.id('registrationMessage'));
+      expect(result.getText()).toEqual('Zoe,Kaylee');
+    });
   });
 });
