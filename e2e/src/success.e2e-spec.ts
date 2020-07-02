878709a4a05328fbee3008549438db75940a99e1
jbarron8 <jbarron8@cs6261-vm28.uwg.westga.edu>
2020-07-02 11:10:49 -0400

    created file to test successful scenario for e2e testing

---
+++ e2e/src/success.e2e-spec.ts
@@ -0,0 +1,10 @@
+import { browser, element, by, logging } from 'protractor';
+
+describe('Successful Scenario Tests', function() {
+  it('should update players array on registration page', function() {
+    browser.get('/');
+    element(by.id('subpageTitle'));
+
+    expect().toEqual();
+  });
+});
