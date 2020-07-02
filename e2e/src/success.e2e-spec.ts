e1cbfc1ccf4a7d634bfa2034364e62ac7ef7bcdc
jbarron8 <jbarron8@cs6261-vm28.uwg.westga.edu>
2020-07-02 12:09:33 -0400

    Test keeps showing undefined. May need to import component to get values to be read

--- e2e/src/success.e2e-spec.ts
+++ e2e/src/success.e2e-spec.ts
@@ -3,8 +3,15 @@ import { browser, element, by, logging } from 'protractor';
 describe('Successful Scenario Tests', function() {
   it('should update players array on registration page', function() {
     browser.get('/');
-    element(by.id('subpageTitle'));
+    element(by.id('registrationLink')).click();
+    var input1 = element(by.model(players[0]));
+    input1.sendKeys('Goku');
 
-    expect().toEqual();
+    var input2 = element(by.model(players[1]));
+    input2.sendKeys('Vegeta');
+
+    var result = element(by.id('contestant1'));
+
+    expect(result.getText()).toEqual("Vegeta");
   });
 });
