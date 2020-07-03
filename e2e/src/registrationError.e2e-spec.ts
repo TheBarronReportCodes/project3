02f9a3a38f9bc33d412e7dff190915e02375573f
jbarron8 <jbarron8@cs6261-vm26.uwg.westga.edu>
2020-07-03 14:55:33 -0400

    Registration page error tests passed. Beginning tests for brackets page errors

--- e2e/src/registrationError.e2e-spec.ts
+++ e2e/src/registrationError.e2e-spec.ts
@@ -12,4 +12,18 @@ describe('Registration Page Errors', function() {
       var result = element(by.id('registrationMessage'));
       expect(result.getText()).toEqual('Should be 2, 4, or 8 contestants');
     });
+
+    it('Message should appear when input is duplicate', function() {
+      browser.get('/');
+      element(by.id('registrationLink')).click();
+
+      var input1 = element(by.id('contestant0'));
+      input1.sendKeys('Pikachu');
+      var input2 = element(by.id('contestant1'));
+      input2.sendKeys('Pikachu');
+      element(by.id('registerContestantsButton')).click();
+
+      var result = element(by.id('registrationMessage'));
+      expect(result.getText()).toEqual('Duplicate player');
+    });
 });
