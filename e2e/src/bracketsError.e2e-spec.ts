738fe8e166034d3b9c4b794c89c77821ba2ac67e
jbarron8 <jbarron8@cs6261-vm21.uwg.westga.edu>
2020-07-04 01:25:43 -0400

    helper functions implemented into bracketErrorspec.ts file

--- e2e/src/bracketsError.e2e-spec.ts
+++ e2e/src/bracketsError.e2e-spec.ts
@@ -1,44 +1,32 @@
-import { browser, element, by } from 'protractor';
+import { Helper } from './models/helper';
 
 describe('Bracket Page Errors', function() {
-  it('Message should show message that all matches must be completed', function() {
-    browser.get('/');
-    element(by.id('registrationLink')).click();
-    var input1 = element(by.id('contestant0'));
-    input1.sendKeys('Pikachu');
-    var input2 = element(by.id('contestant1'));
-    input2.sendKeys('Ash');
-    element(by.id('registerContestantsButton')).click();
-    element(by.id('bracketsLink')).click();
-    element(by.id('completeRoundButton')).click();
+  let helper = new Helper();
 
-    var result = element(by.id('bracketsMessage'));
-    expect(result.getText()).toEqual('Please complete all matches');
+  it('Message should show message that all matches must be completed', function() {
+    helper.loadHome();
+    helper.idClick('registrationLink');
+    helper.idSendKeys('contestant0', 'Pikachu');
+    helper.idSendKeys('contestant1', 'Ash');
+    helper.idClick('registerContestantsButton');
+    helper.idClick('bracketsLink');
+    helper.idClick('completeRoundButton');
+    helper.idExpectToEqual('bracketsMessage', 'Please complete all matches');
     });
 
-    it('Message should show message that all matches must be completed test two', function() {
-      browser.get('/');
-      element(by.id('registrationLink')).click();
-      element(by.id('autofillEightButton')).click();
-      element(by.id('registerContestantsButton')).click();
-      element(by.id('bracketsLink')).click();
+  it('Message should show message that all matches must be completed test two', function() {
+      helper.loadHome();
+      helper.idClick('registrationLink');
+      helper.idClick('autofillEightButton');
+      helper.idClick('registerContestantsButton');
+      helper.idClick('bracketsLink');
 
-      var contestant1Match1Round1 = element(by.id('contestant1Match1'));
-      var contestant2Match1Round1 = element(by.id('contestant2Match1'));
-      var contestant1Match2Round1 = element(by.id('contestant1Match2'));
-      var contestant2Match2Round1 = element(by.id('contestant2Match2'));
-      var contestant1Match3Round1 = element(by.id('contestant1Match3'));
-      var contestant2Match3Round1 = element(by.id('contestant2Match3'));
-      var contestant1Match4Round1 = element(by.id('contestant1Match4'));
-      var contestant2Match4Round1 = element(by.id('contestant2Match4'));
+      helper.idClick('contestant1Match1');
+      helper.idClick('contestant2Match2');
+      helper.idClick('contestant2Match3');
 
-      contestant1Match1Round1.click();
-      contestant2Match2Round1.click();
-      contestant2Match3Round1.click();
-
-      element(by.id('completeRoundButton')).click();
-
-      var result = element(by.id('bracketsMessage'));
-      expect(result.getText()).toEqual('Please complete all matches');
-      });
+      helper.idClick('completeRoundButton');
+      helper.idExpectToEqual('bracketsMessage', 'Please complete all matches');
+    });
+    
   });
