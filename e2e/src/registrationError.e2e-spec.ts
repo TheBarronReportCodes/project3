2ce3794ab45c05e81ffefed58bb59edcab170087
jbarron8 <jbarron8@cs6261-vm21.uwg.westga.edu>
2020-07-04 01:35:35 -0400

    refactored registrationError.e2e-spec.ts file. final file to refactor is success.e2e-spec.ts

--- e2e/src/registrationError.e2e-spec.ts
+++ e2e/src/registrationError.e2e-spec.ts
@@ -1,29 +1,23 @@
-import { browser, element, by } from 'protractor';
+import { Helper } from './models/helper';
 
 describe('Registration Page Errors', function() {
+  let helper = new Helper();
     it('Message should appear if registration is not of length 2, 4, or 8', function() {
-      browser.get('/');
-      element(by.id('registrationLink')).click();
+      helper.loadHome();
+      helper.idClick('registrationLink');
+      helper.idSendKeys('contestant0', 'Pikachu');
 
-      var input1 = element(by.id('contestant0'));
-      input1.sendKeys('Pikachu');
-      element(by.id('registerContestantsButton')).click();
-
-      var result = element(by.id('registrationMessage'));
-      expect(result.getText()).toEqual('Should be 2, 4, or 8 contestants');
+      helper.idClick('registerContestantsButton');
+      helper.idExpectToEqual('registrationMessage', 'Should be 2, 4, or 8 contestants');
     });
 
     it('Message should appear when input is duplicate', function() {
-      browser.get('/');
-      element(by.id('registrationLink')).click();
+      helper.loadHome();
+      helper.idClick('registrationLink');
+      helper.idSendKeys('contestant0', 'Pikachu');
+      helper.idSendKeys('contestant1', 'Pikachu');
 
-      var input1 = element(by.id('contestant0'));
-      input1.sendKeys('Pikachu');
-      var input2 = element(by.id('contestant1'));
-      input2.sendKeys('Pikachu');
-      element(by.id('registerContestantsButton')).click();
-
-      var result = element(by.id('registrationMessage'));
-      expect(result.getText()).toEqual('Duplicate player');
+      helper.idClick('registerContestantsButton');
+      helper.idExpectToEqual('registrationMessage', 'Duplicate player');
     });
 });
